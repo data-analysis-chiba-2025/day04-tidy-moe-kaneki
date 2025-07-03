5442d24e1b1c3c8e504f05cad1c708753f519e43
moe kaneki <24zb1089@student.gs.chiba-u.jp>
2025-07-03 15:13:36 +0900

    Moe Kaneki

--- day04_homework.R
+++ day04_homework.R
@@ -1,6 +1,6 @@
 # DAY 4 HOMEWORK
 #
-# Name:
+# Name: Moe Kaneki
 
 # For this week's homework, the answer to each question should be a dataframe
 # (tibble)
@@ -10,23 +10,37 @@
 # HINT: This repo includes the gapminder.csv file in the data_raw folder
 # HINT: I've written some of the code for you.
 
-gapminder <-
+library(tidyverse)
+gapminder <- read.csv("data_raw/gapminder.csv")
 
 # 2. Sort the data so that the country with the highest per capita GDP
 # (gdpPercap) is at the top. Save the result to answer_2
 
+answer2 <- gapminder |>
+  arrange(desc(gdpPercap))
+  
 
 # 3. Filter the gapminder data to the most recent year in the
 # dataset, then select only the country and population columns. DO NOT use the
 # pipe for this. Save the result to answer_3
 
+gapminder_year <- arrange(gapminder, desc(year))
+answer_3 <- select(gapminder_year, country, pop)
 
-# 4. Answer question 4 again, but this time USE the pipe (|>). Save the
+# 4. Answer question 4 again, byear()# 4. Answer question 4 again, but this time USE the pipe (|>). Save the
 # result to answer_4
 
+answer_4 <- gapminder |>
+  arrange(desc(year)) |>
+  select(country, pop)
 
 # 5. Calculate the mean life expectancy for each country in
 # Asia only. Save the result to answer_5
 #
 # HINT: use the combination of `group_by()` and `summarize()` to calculate
 # a mean value by group
+
+answer_5 <- gapminder |>
+  filter(continent == "Asia") |>
+  group_by(country) |>
+  summarize(mean_life_expectancy = mean(lifeExp))
